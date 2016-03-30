a7cd801bda87af836a304894774c70b8ab61c677
Sourabh Deshkulkarni <sourabh_deshkulkarni@sourabhs-macbook-pro.local>
2016-03-30 09:34:16 -0400

    Initial Commit

---
+++ FavoritePlaces/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FavoritePlaces
+//
+//  Created by Sourabh Deshkulkarni on 3/30/16.
+//  Copyright © 2016 Sourabh Deshkulkarni. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
